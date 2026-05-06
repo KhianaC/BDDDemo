7642290086961d9763e1b80eefb0a315b45d3583
KhianaC <priyankaarora5990@gmail.com>
2026-05-06 23:34:19 +0530

    CRS-123 committing gitignore file

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-   public static void main(String[] args) {
-        System.out.println("This is my first github commit integrating with jenkinsssss");
+    static void main() {
+        System.out.println("This is my first github commit integrating with jenkins");
     }
 }
