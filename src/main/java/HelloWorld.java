4c389d3a8cc90697eaeb0faeb494c6e9b8a1d470
KhianaC <priyankaarora5990@gmail.com>
2026-06-01 01:11:33 +0530

    Fix typo in output message for HelloWorld

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     static void main() {
-        System.out.println("This is my first github commit integrating with jenkins");
+        System.out.println("This is my first github commit integrating with jenkin");
         System.out.println("This is commit from feature into master");
         System.out.println("This demo for no conflict");
         System.out.println("This demo ");
