2c65b27b1ff1c09e2c60916022ff9598a8d3f319
KhianaC <priyankaarora5990@gmail.com>
2026-05-31 21:18:22 +0530

    fixing the broken build
    Fix typo in method declaration from 'staticc' to 'static'.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    staticc void main() {
+    static void main() {
         System.out.println("This is my first github commit integrating with jenkins");
         System.out.println("This is commit from feature into master");
         System.out.println("This demo for no conflict");
